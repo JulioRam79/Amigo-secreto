0e39f5fe4c4335377189bc10a1589bb77fe5130d
Julio Ramirez <cesar_ramirez87@outlook.es>
2025-02-12 02:27:16 -0600

    Se actualiza app.js para agregar nuevos amigos

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaDeNombres = [];
+
+
+function agregarAmigo() {
+    let nuevoNombre = document.getElementById('amigo').value;
+    if (nuevoNombre == ""){
+        alert('Ingresa un nombre valido');
+    }
+    else{
+        if(listaDeNombres.includes(nuevoNombre)){
+            alert('Nombre repetido');
+            limpiarCaja();
+        }else{
+            listaDeNombres.push(nuevoNombre);
+            limpiarCaja();
+            console.log(listaDeNombres);
+        }
+        modificarTextos('#listaAmigos', listaDeNombres);  
+    }
+}
+
+function limpiarCaja (){
+    document.getElementById('amigo').value = "";
+}
+        
+function modificarTextos (elemento, texto){
+    let textoNuevo = document.querySelector(elemento);
+    textoNuevo.innerHTML = texto.map(item => `<p>${item}</p>`).join("");
+}
+
+
